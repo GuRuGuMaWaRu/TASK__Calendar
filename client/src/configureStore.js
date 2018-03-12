f298a3ba96a5d3462fba4ad4ad5fffe240a477e9
Peter Krevenets <peter.k.nets@gmail.com>
2018-03-12 09:33:46 +0200

    Remove commenting out from redux devtools line

--- client/src/configureStore.js
+++ client/src/configureStore.js
@@ -1,19 +1,23 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import {
+  createStore,
+  applyMiddleware,
+  compose
+} from "redux";
 import thunk from "redux-thunk";
 
 import rootReducer from "./reducers";
 
 export const configureStore = () => {
-  // if (process.env.MODE_ENV !== "production") {
-  //   return createStore(
-  //     rootReducer,
-  //     compose(
-  //       applyMiddleware(thunk),
-  //       window.__REDUX_DEVTOOLS_EXTENSION__ &&
-  //         window.__REDUX_DEVTOOLS_EXTENSION__()
-  //     )
-  //   );
-  // }
+  if (process.env.NODE_ENV !== "production") {
+    return createStore(
+      rootReducer,
+      compose(
+        applyMiddleware(thunk),
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+      )
+    );
+  }
 
   return createStore(rootReducer, applyMiddleware(thunk));
 };
